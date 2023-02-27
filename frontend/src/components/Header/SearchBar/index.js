3be740de4e220e848ee121a3ee0c235ccfc6febf
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-02-27 11:38:22 -0300

    feat: searchbar is now functional and integrated with the API

--- frontend/src/components/Header/SearchBar/index.js
+++ frontend/src/components/Header/SearchBar/index.js
@@ -1,26 +1,53 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { 
 	Container,
 	ResultList,
 	Result,
 	ProductInfo,
- } from './styles.js'
-import magnifierImg from '../../../assets/label/magnifier.svg'
+ } from './styles.js';
+import magnifierImg from '../../../assets/label/magnifier.svg';
+import { useProducts } from '../../../hooks/useProducts';
+import { currencyFormatter } from '../../../utils/currencyFormatter';
 
 export function SearchBar({ isSearchBarOnFocus = false, handleSearchBarFocus = () => {}}) {
+	const [searchbarValue, setSearchbarValue] = useState('');
+	const [productsMatches, setProductsMatches] = useState([]);
+	const products = useProducts();
+
+	const handleSearchProducts = (event) =>  setSearchbarValue(event.target.value);
+
+	useEffect(() => {
+		if(searchbarValue.trim().length > 0) {
+			const matches = products.filter(product => product.product.toLowerCase().includes(searchbarValue.toLowerCase().trim()));
+
+			setProductsMatches(matches);
+		} else {
+			setProductsMatches([]);
+		}
+	}, [searchbarValue, products]);
+
 	return (
 		<Container>
 			<img src={magnifierImg}/>
 			<label className="sr-only" htmlFor="search-engine">Pesquise aqui seu produto</label>
-			<input type="text" placeholder="Pesquisar produto" name="product-name" id="search-engine" autoComplete="off" onFocus={() => handleSearchBarFocus(true)} onBlur={() => handleSearchBarFocus(false)}/>
+			<input 
+				type="text" 
+				placeholder="Pesquisar produto" 
+				name="product-name" 
+				id="search-engine" 
+				value={searchbarValue} 
+				onChange={handleSearchProducts} 
+				autoComplete="off" 
+				onFocus={() => handleSearchBarFocus(true)} 
+				onBlur={() => handleSearchBarFocus(false)}/>
 
 			{isSearchBarOnFocus && <ResultList>
-				{new Array(8).fill(0).map((i, index) => (
-					<Result key={index}>
-						<img src="images/vboard-rtx-4.jpg" loading="lazy"/>
+				{productsMatches.map((product) => (
+					<Result key={product.id}>
+						<img src={`products/${product.product_category}/${product.icon_reference}.png`} loading="lazy"/>
 						<ProductInfo>
-							<span>Placa de Vídeo Galax GeForce RTX 4090 ST, 24GB GDDR6X, ARGB, DLSS, Ray Tracing</span>
-							<strong>R$ 14.999,99</strong>
+							<span>{product.product}</span>
+							<strong>{currencyFormatter(product.price)}</strong>
 						</ProductInfo>
 					</Result>
 				))}
