649a1d5a14f3b83acac5999539530c3c242a39e9
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-03-07 22:11:53 -0300

    feat: Product data and logic updated with new liked property.

--- frontend/src/providers/productsProvider.js
+++ frontend/src/providers/productsProvider.js
@@ -1,11 +1,12 @@
 import { useState, useEffect, createContext } from 'react';
 import { api } from '../services/api';
+import Storage from '../utils/storage';
 
 export const ProductsContext = createContext([]);
 const urlRegex = /[ ,]+/g;
 
 export function ProductsProvider({ children }) {
-	const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState(() => Storage.get('@products'));
 	const [selectedProduct, setSelectedProduct] = useState({});
 
 	useEffect(() => {
@@ -13,7 +14,6 @@ export function ProductsProvider({ children }) {
 			try {
 				const apiResponse = await api.get('/products');
 				const jsonData = apiResponse.data;
-
 				setProducts(jsonData);
 			} catch(err) {
 				console.error(err.message);
@@ -21,8 +21,25 @@ export function ProductsProvider({ children }) {
 		})()
 	}, [])
 
+
+
+	const handleLikeProduct = async (id) => {
+	  setProducts(prevState => prevState.map(product => {
+	    if (product.id === id) {
+	      	return {
+	        	...product,
+	        	liked: !product.liked
+	      	};
+	    }
+	    return product;
+	  }));
+
+	  const product = products.find(product => product.id === id);
+	  await api.patch(`/products/liked/${id}?liked=${!product.liked}`);	
+	};
+
 	return (
-		<ProductsContext.Provider value={products}>
+		<ProductsContext.Provider value={{products, handleLikeProduct}}>
 			{ children }
 		</ProductsContext.Provider>
 	)
