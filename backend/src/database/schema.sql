d1cf4dd70f01eacf183083aab6c91545cad5c5c7
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-03-12 21:14:38 -0300

    users table added

--- backend/src/database/schema.sql
+++ backend/src/database/schema.sql
@@ -12,3 +12,9 @@ CREATE TABLE IF NOT EXISTS products(
   product_category VARCHAR,
   liked BOOLEAN
 );
+
+CREATE TABLE IF NOT EXISTS users(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+);
