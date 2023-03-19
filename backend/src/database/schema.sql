c629d2d89807bea7b19bbb1ec72a58f31c9591a4
Igor Figueiredo <igorfigueiredors@gmail.com>
2023-03-18 22:10:42 -0300

    feat: table favorites created

--- backend/src/database/schema.sql
+++ backend/src/database/schema.sql
@@ -14,7 +14,12 @@ CREATE TABLE IF NOT EXISTS products(
 );
 
 CREATE TABLE IF NOT EXISTS users(
-  id SERIAL PRIMARY KEY,
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   username VARCHAR(50) NOT NULL,
-  password VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS favorites(
+  user_id INTEGER NOT NULL REFERENCES users(id),
+  product JSONB NOT NULL
 );
